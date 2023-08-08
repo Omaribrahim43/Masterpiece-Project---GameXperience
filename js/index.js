4bd360a79769993e475aaec6564b4dbf58942ca0
Omaribrahim43 <omar.migdadi43@gmail.com>
2023-08-08 17:54:14 +0300

    navbar modified

--- js/index.js
+++
@@ -1,9 +0,0 @@
-const navLinks = document.querySelectorAll(".navbar ul li");
-
-navLinks.forEach((navLink) => {
-  navLink.addEventListener("click", () => {
-    document.querySelector(".active")?.classList.remove("active");
-    navLink.classList.add("active");
-  });
-});
-
