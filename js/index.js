c1beda5f26e052d189ef66c715e928c8cc6e067e
Omaribrahim43 <omar.migdadi43@gmail.com>
2023-08-09 20:28:02 +0300

    modified

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// window.addEventListener("scroll", function () {
+//   var landing = document.querySelector(".landing-page");
+//   var navbar = document.querySelector(".navbar");
+
+//   if (landing.getBoundingClientRect().top <= 0) {
+//     navbar.classList.add("scrolled-background");
+//   } else {
+//     navbar.classList.remove("scrolled-background");
+//   }
+// });
